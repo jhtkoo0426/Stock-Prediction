bc2c8ea14101317208be1aa4612e881f035dde80
jhtkoo0426 <jhtkoo@gmail.com>
2022-08-29 20:41:34 +0100

    Initialized training branch

---
+++ src/stock_prediction.py
@@ -0,0 +1,15 @@
+"""
+Training a model to predict the price trend for a single stock.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def plot_series(time, series, format="-", start=0, end=None, label=None):
+    plt.plot(time[start:end], series[start:end], format, label=label)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    if label:
+        plt.legend(fontsize=14)
+    plt.grid(True)
+
