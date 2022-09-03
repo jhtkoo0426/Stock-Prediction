8ea9d29e8b2403e2b363855b26d7a53b9cc24226
jhtkoo0426 <jhtkoo@gmail.com>
2022-09-03 20:14:01 +0100

    Refactored code into reusable functions

--- src/stock_prediction.py
+++ src/stock_prediction.py
@@ -21,112 +21,131 @@ FINNHUB_API_KEY = os.environ.get('FINNHUB_API_KEY')
 
 
 # Download historical data for one stock symbol from Finnhub.
-start = time.mktime(datetime.datetime.strptime('01/01/2000', '%d/%m/%Y').timetuple())
-end = time.time()
-fh_client = fh.Client(api_key=FINNHUB_API_KEY)
-candles_dict = fh_client.stock_candles('aapl', 'D', int(start), int(end))
-df = pd.DataFrame(candles_dict)
-print(df)
-print(df.shape)
-
-
-# Visualise closing price history
-plt.figure()
-plt.title('Closing price history of aapl')
-plt.plot(df['c'])
-plt.xlabel('Date')
-plt.ylabel('Close Price USD ($)')
-plt.show()
+def download_stock_data(symbol):
+    start = time.mktime(datetime.datetime.strptime('01/01/2000', '%d/%m/%Y').timetuple())
+    end = time.time()
+    fh_client = fh.Client(api_key=FINNHUB_API_KEY)
+    candles_dict = fh_client.stock_candles(symbol, 'D', int(start), int(end))
+    stock_df = pd.DataFrame(candles_dict)
+    # print(stock_df.shape)
+    return stock_df
 
 
 # Create a new dataframe with only the closing column
-data = df.filter(['c'])
-dataset = data.values       # Convert to np.array
-print(dataset.shape)
-training_data_len = math.ceil(len(dataset) * .8)
-print(training_data_len)
+def get_closing_column(stock_df):
+    col = stock_df.filter(['c'])
+    col_arr = col.values       # Convert to np.array
+    # print(dataset.shape)
+    # print(dataset)
+    return col, col_arr
 
 
-# Scale the data
-scaler = MinMaxScaler(feature_range=(0,1))
-scaled_data = scaler.fit_transform(dataset)
-print(scaled_data)
+# Calculate the amount of training data
+def calc_training_data_len(dataset):
+    length = math.ceil(len(dataset) * .8)
+    # print(length)
+    return length
+
+
+def create_scaler():
+    min_scaler = MinMaxScaler(feature_range=(0, 1))
+    return min_scaler
+
+
+# Scale the data for training
+def rescale(dataset, min_scaler):
+    rescaled_data = min_scaler.fit_transform(dataset)
+    # print(rescaled_data)
+    return rescaled_data
 
 
 # Create the training dataset
-training_data = scaled_data[0:training_data_len, :]
-x_train, y_train = [], []       # Split the data into x_train and y_train datasets
+def build_training_ds(dataset, training_data_len):
+    train = dataset[0:training_data_len, :]
+    x_train, y_train = [], []       # Split the data into x_train and y_train datasets
 
-for i in range(60, len(training_data)):
-    x_train.append(training_data[i-60:i, 0])        # Contains 60 values
-    y_train.append(training_data[i, 0])
-    if i <= 60:
-        print(x_train)
-        print(y_train)
-        print()
+    for i in range(60, len(train)):
+        x_train.append(train[i - 60:i, 0])  # Contains 60 values
+        y_train.append(train[i, 0])
 
+    x_train, y_train = np.array(x_train), np.array(y_train)
 
-# Convert training datasets to np arrays
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-# Reshape the x training dataset to fit LSTM model
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))        # No. rows, no. of columns, 1
-print(x_train.shape)
-
-
-# Build LSTM model
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(LSTM(50, return_sequences=False))
-model.add(Dense(25))
-model.add(Dense(1))
-model.compile(optimizer='adam', loss='mean_squared_error')          # Compile the model
-
-
-# Train the model
-model.fit(x_train, y_train, batch_size=1, epochs=1)
+    # Reshape the x training dataset to fit LSTM model
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))  # No. rows, no. of columns, 1
+    return x_train, y_train
 
 
 # Create the testing dataset
-testing_dataset = scaled_data[training_data_len-60:, :]
-x_test = []
-y_test = dataset[training_data_len:, :]
-for i in range(60, len(testing_dataset)):
-    x_test.append(testing_dataset[i-60:i, 0])
+def build_testing_ds(dataset, length):
+    # Create the testing dataset
+    testing_dataset = dataset[length - 60:, :]
+    x_testing = []
+    y_testing = dataset[length:, :]
+    for i in range(60, len(testing_dataset)):
+        x_testing.append(testing_dataset[i - 60:i, 0])
+
+    # Convert data to np.array
+    x_testing = np.array(x_testing)
+    x_testing = np.reshape(x_testing, (x_testing.shape[0], x_testing.shape[1], 1))
+    return x_testing, y_testing
 
 
-# Convert data to np.array
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+# Build and train LSTM model
+def run_model(X_train, Y_train):
+    # Build LSTM model
+    model = Sequential()
+    model.add(LSTM(50, return_sequences=True, input_shape=(X_train.shape[1], 1)))
+    model.add(LSTM(50, return_sequences=False))
+    model.add(Dense(25))
+    model.add(Dense(1))
+    model.compile(optimizer='adam', loss='mean_squared_error')  # Compile the model
+
+    # Train the model
+    model.fit(X_train, Y_train, batch_size=60, epochs=100)
+    return model
 
 
-# Get the model's predicted price values
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)         # Un-scaling the transformed values
+# Main function
+def build_and_predict(symbol):
+    stock_data = download_stock_data(symbol)
+    col, close_price_dataset = get_closing_column(stock_data)
+    training_data_len = calc_training_data_len(close_price_dataset)
+
+    scaler = create_scaler()
+    scaled_data = rescale(close_price_dataset, scaler)
+
+    xt, yt = build_training_ds(scaled_data, training_data_len)
+    trained_model = run_model(xt, yt)
+
+    x_test, y_test = build_testing_ds(scaled_data, training_data_len)
+
+    # Get the model's predicted price values
+    predictions = trained_model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)         # Un-scaling the transformed values
+
+    # Get the RMSE
+    rmse = np.sqrt(np.mean(predictions-y_test)**2)
+    print("RMSE: ", rmse)
+
+    plot_results(col, training_data_len, predictions)
 
 
-# Get the RMSE
-rmse = np.sqrt(np.mean(predictions-y_test)**2)
-print(rmse)
+def plot_results(col, length, predictions):
+    # Prepare results for plot
+    train_data_points = col[:length]
+    validation_data_points = col[length:]
+    validation_data_points['Predictions'] = predictions
+
+    # Plot results
+    plt.figure(figsize=(16, 8))
+    plt.title('Stock prediction for aapl using LSTM')
+    plt.xlabel('Time')
+    plt.ylabel('Price of aapl in USD ($)')
+    plt.plot(train_data_points['c'])
+    plt.plot(validation_data_points[['c', 'Predictions']])
+    plt.legend(['Train', 'Validation', 'Predictions'], loc='lower right')
+    plt.grid()
+    plt.show()
 
 
-# Prepare results for plot
-train_data_points = data[:training_data_len]
-validation_data_points = data[training_data_len:]
-validation_data_points['Predictions'] = predictions
-
-# Plot results
-plt.figure(figsize=(16, 8))
-plt.title('Stock prediction for aapl using LSTM')
-plt.xlabel('Time')
-plt.ylabel('Price of aapl in USD ($)')
-plt.plot(train_data_points['c'])
-plt.plot(validation_data_points[['c', 'Predictions']])
-plt.legend(['Train', 'Validation', 'Predictions'], loc='lower right')
-plt.grid()
-plt.show()
-
-
-# Visualising results via terminal
-print("Validation results")
-print(validation_data_points)
+build_and_predict('aapl')
