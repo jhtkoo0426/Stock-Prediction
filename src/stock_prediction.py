71ef2e8b47786ac5492fa17e838e6d12281ee81c
jhtkoo0426 <jhtkoo@gmail.com>
2022-09-03 12:19:28 +0100

    Created & trained model for aapl

--- src/stock_prediction.py
+++ src/stock_prediction.py
@@ -73,3 +73,60 @@ x_train, y_train = np.array(x_train), np.array(y_train)
 x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))        # No. rows, no. of columns, 1
 print(x_train.shape)
 
+
+# Build LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+model.compile(optimizer='adam', loss='mean_squared_error')          # Compile the model
+
+
+# Train the model
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+
+# Create the testing dataset
+testing_dataset = scaled_data[training_data_len-60:, :]
+x_test = []
+y_test = dataset[training_data_len:, :]
+for i in range(60, len(testing_dataset)):
+    x_test.append(testing_dataset[i-60:i, 0])
+
+
+# Convert data to np.array
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+
+# Get the model's predicted price values
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)         # Un-scaling the transformed values
+
+
+# Get the RMSE
+rmse = np.sqrt(np.mean(predictions-y_test)**2)
+print(rmse)
+
+
+# Prepare results for plot
+train_data_points = data[:training_data_len]
+validation_data_points = data[training_data_len:]
+validation_data_points['Predictions'] = predictions
+
+# Plot results
+plt.figure(figsize=(16, 8))
+plt.title('Stock prediction for aapl using LSTM')
+plt.xlabel('Time')
+plt.ylabel('Price of aapl in USD ($)')
+plt.plot(train_data_points['c'])
+plt.plot(validation_data_points[['c', 'Predictions']])
+plt.legend(['Train', 'Validation', 'Predictions'], loc='lower right')
+plt.grid()
+plt.show()
+
+
+# Visualising results via terminal
+print("Validation results")
+print(validation_data_points)
